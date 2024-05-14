9aaa84f08dbbcbb3432efce441bc6690f1872af5
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-05-14 14:08:23 +0530

    Committed changes in reverseanarray file

--- reverseanarray.java
+++ reverseanarray.java
@@ -2,12 +2,24 @@ import java.util.*;
 
 public class reverseanarray {
     public static void reversearr(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[end];
 
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        sc.close();
+        int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("The new array is : ");
     }
 }
