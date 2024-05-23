03aa33f46ad61495ffa8572f15dd26562667f91b
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-05-23 19:45:06 +0530

    printed numbers to 10

---
+++ onetoten.java
@@ -0,0 +1,13 @@
+// Counting to 1 to 10
+public class onetoten {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+
+            if (i == 11) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("The code is printed till 10");
+    }
+}
