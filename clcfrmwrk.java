02a8829ab36b774d6d9fcbb60e0d45a5fffff7f2
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-06-17 13:32:32 +0530

    A new Branch created and a few topics of DSA to be executed in them.

---
+++ clcfrmwrk.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class clcfrmwrk {
+    public static void main(String[] args) {
+        // String [] studentname = new String[50];
+        // studentname[0] = "agrim";
+        // studentname[1] = "akshay";
+        // studentname[2] = "ajay";
+        // studentname[3] = "abhinn";
+        // studentname[4] = "aditya";
+        // studentname[5] = "aman";
+
+        ArrayList<String> studentname = new ArrayList<>();
+        studentname.add("Prakash");
+        studentname.add("Agrim");
+        studentname.add("akash");
+        studentname.add("Prakrit");
+        studentname.add("Pratyush");
+        System.out.println(studentname);
+        // A thing to note in this is that it uses import java util to execute, dont
+        // forget to mention those when you write the code.
+
+    }
+}
